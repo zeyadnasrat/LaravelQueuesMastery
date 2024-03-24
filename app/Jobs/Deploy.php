0bf3e8c4e0142c0c21f4c8658993e4510ae019bf
Zeyad Nasrat <zeyad7nasrat@gmail.com>
2024-03-24 13:26:37 +0200

    Done

---
+++ app/Jobs/Deploy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\ThrottlesExceptions;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+
+class Deploy implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        info('Start Deploy');
+
+        throw \Exception("Failed");
+
+        sleep(5);
+
+        info('Deploy ended');
+
+    }
+
+    public function middleware()
+    {
+        return  [
+            new ThrottlesExceptions(2),
+        ];
+    }
+}
---
+++ app/Jobs/SendWelcomeEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use mysql_xdevapi\Exception;
+
+class SendWelcomeEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public $tries = 10;
+    public $maxExceptions = 2;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        throw new Exception("failed!");
+        return $this->release();
+    }
+
+    public function failed($e)
+    {
+        info('Failed!!!!!!!!');
+    }
+}
